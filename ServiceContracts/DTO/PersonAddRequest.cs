179a31339092fa5531c3b856cb2d042819a5d2c2
ahmet <80220613+ErgunAhmet@users.noreply.github.com>
2023-07-03 18:52:32 +0200

    taghelpers

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -15,10 +15,13 @@ namespace ServiceContracts.DTO
 
 		[Required(ErrorMessage = "Email can't be blank")]
 		[EmailAddress(ErrorMessage = "Email value should be a valid email")]
+		[DataType(DataType.EmailAddress)]
 		public string? Email { get; set; }
 
 		public DateTime? DateOfBirth { get; set; }
+		[Required(ErrorMessage ="Gender cannot be blank")]
 		public GenderOptions? Gender { get; set; }
+		[Required(ErrorMessage ="Country required")]
 		public Guid? CountryID { get; set; }
 		public string? Address { get; set; }
 		public bool ReceiveNewsLetters { get; set; }
